03b34add832e717e56a278b85790195cb4ff35ae
Vuyani2 <vuyanikunelisi@gmail.com>
2021-06-08 14:45:11 +0200

    updated main and test

---
+++ main.py
@@ -0,0 +1,14 @@
+# Mini Calculator App
+
+class MiniCalculator():
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiplication(self, a, b):
+        return a * b
+
+    def divition(self, a, b):
+        return a/b
